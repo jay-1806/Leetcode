205a4e9b639822b2e69ef2bc8ed8a580f7b30276
jay-1806 <136622277+jay-1806@users.noreply.github.com>
2025-10-02 16:17:16 -0700

    Time: 4 ms (1.24%), Space: 18.2 MB (72.43%) - LeetHub

---
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
@@ -0,0 +1,37 @@
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        if not matrix or not matrix[0]:
+            return False
+        
+        rows = len(matrix)
+        cols = len(matrix[0])
+
+        left = 0
+        right = (rows * cols) - 1
+
+        while left <= right:
+            mid = left + (right - left) // 2 
+
+            r = mid // cols
+            c = mid % cols
+
+            mid_value = matrix[r][c]
+
+            if mid_value == target:
+                return True
+            elif mid_value > target:
+                right = mid - 1
+            else:
+                left = mid + 1
+                
+        return False
+
+
+
+
+
+
+
+
+
+        
