d017a3ce4085c11b6d4a4b2a72f9a8d08872fe98
jay-1806 <136622277+jay-1806@users.noreply.github.com>
2025-10-03 21:03:57 -0700

    Time: 127 ms (49.53%), Space: 75.3 MB (17.12%) - LeetHub

--- 1023-time-based-key-value-store/1023-time-based-key-value-store.py
+++ 1023-time-based-key-value-store/1023-time-based-key-value-store.py
@@ -1,28 +1,28 @@
 class TimeMap:
+
     def __init__(self):
-        # We store the data more optimally for bisect:
-        # key -> [ [timestamps], [values] ]
-        # This keeps the two lists parallel and avoids list-of-list lookups
-        self.store = collections.defaultdict(lambda: [[], []])
+        self.keyStore = {}  # key : list of [val, timestamp]
 
     def set(self, key: str, value: str, timestamp: int) -> None:
-        # Append to the parallel lists. O(1)
-        self.store[key][0].append(timestamp)
-        self.store[key][1].append(value)
+        if key not in self.keyStore:
+            self.keyStore[key] = []
+        self.keyStore[key].append([value, timestamp])
 
     def get(self, key: str, timestamp: int) -> str:
-        if key not in self.store:
-            return ""
-
-        timestamps, values = self.store[key]
+        res, values = "", self.keyStore.get(key, [])
+        l, r = 0, len(values) - 1
+        while l <= r:
+            m = (l + r) // 2
+            if values[m][1] <= timestamp:
+                res = values[m][0]
+                l = m + 1
+            else:
+                r = m - 1
+        return res
         
-        # O(log L) operation using the optimized C implementation
-        # 'i' is the index *after* the largest timestamp <= the target
-        i = bisect.bisect_right(timestamps, timestamp)
 
-        # Case 1: i == 0 means all timestamps are > target_timestamp (i.e., too early)
-        if i == 0:
-            return ""
-        
-        # Case 2: The largest valid timestamp is at index i - 1
-        return values[i - 1]
+
+# Your TimeMap object will be instantiated and called as such:
+# obj = TimeMap()
+# obj.set(key,value,timestamp)
+# param_2 = obj.get(key,timestamp)
