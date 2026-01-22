963128ba1b7b75730f2a9302165c0233a95cb39d
jay-1806 <136622277+jay-1806@users.noreply.github.com>
2026-01-21 22:26:02 -0500

    Time: 0 ms (100%), Space: 19.6 MB (6.08%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.py
+++ 0091-decode-ways/0091-decode-ways.py
@@ -7,14 +7,13 @@ class Solution:
                 return dp[i]
             if s[i] == "0":
                 return 0
-
-            res = dfs(i + 1)
-            if i + 1 < len(s) and (
-                s[i] == "1" or s[i] == "2" and
-                s[i + 1] in "0123456"
-            ):
-                res += dfs(i + 2)
+            
+            res = dfs(i+1)
+            if i+1 < len(s) and (
+                s[i] == "1" or 
+                s[i] == "2" and s[i+1] < '7'):
+                res += dfs(i+2)
+            
             dp[i] = res
             return res
-
         return dfs(0)
